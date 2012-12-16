e5e85f6f7120265f188189d98106d3ca6aeed4e2
Domizio Demichelis <dd.nexus@gmail.com>
2012-12-16 10:15:06 +0100

    removed deep_merge before query calls

--- lib/flex/scope/query_methods.rb
+++ lib/flex/scope/query_methods.rb
@@ -23,29 +23,26 @@ module Flex
         raise ArgumentError, "Empty argument passed (got #{ids.inspect})" \
               if ids.nil? || ids.respond_to?(:empty?) && ids.empty?
         wrapped = ids.is_a?(::Array) ? ids : [ids]
-        result = Query.ids deep_merge(*vars, :ids => wrapped)
+        result  = Query.ids self, *vars, :ids => wrapped
         (ids.is_a?(::Array) || result.variables[:raw_result]) ? result : result.first
       end
 
       # it limits the size of the query to the first document and returns it as a single document object
       def first(*vars)
-        variables = params(:size => 1).deep_merge(*vars)
-        result    = Query.get(variables)
+        result = Query.get params(:size => 1), *vars
         result.variables[:raw_result] ? result : result.first
       end
 
       # it limits the size of the query to the last document and returns it as a single document object
       def last(*vars)
-        variables = params(:from => count-1, :size => 1).deep_merge(*vars)
-        result    = Query.get(variables)
+        result = Query.get params(:from => count-1, :size => 1), *vars
         result.variables[:raw_result] ? result : result.first
       end
 
       # will retrieve all documents, the results will be limited by the default :size param
       # use #scan_all if you want to really retrieve all documents (in batches)
       def all(*vars)
-        variables = deep_merge(*vars)
-        Query.get variables
+        Query.get self, *vars
       end
 
       def each(*vars, &block)
@@ -53,22 +50,19 @@ module Flex
       end
 
       def destroy(*vars)
-        variables = deep_merge(*vars)
-        Query.destroy variables
+        Query.destroy self, *vars
       end
 
       # scan_search: the block will be yielded many times with an array of batched results.
       # You can pass :scroll and :size as params in order to control the action.
       # See http://www.elasticsearch.org/guide/reference/api/search/scroll.html
       def scan_all(*vars, &block)
-        variables = deep_merge(*vars)
-        Query.flex.scan_search(:get, variables, &block)
+        Query.flex.scan_search(:get, self, *vars, &block)
       end
 
       # performs a count search on the scope
       def count(*vars)
-        variables = deep_merge(*vars)
-        result    = Query.flex.count_search(:get, variables)
+        result = Query.flex.count_search(:get, self, *vars)
         result['hits']['total']
       end
 
