e073ca1958a22e8ec8a7a2922c5836cb8297fdf5
Domizio Demichelis <dd.nexus@gmail.com>
2013-01-05 09:47:43 +0100

    better get_docs

--- lib/flex/result/scope.rb
+++ lib/flex/result/scope.rb
@@ -4,14 +4,7 @@ module Flex
 
       def get_docs
         return self if variables[:raw_result]
-        case self
-        when Flex::Result::Search
-          self['hits']['hits']
-        when Flex::Result::MultiGet
-          self['docs']
-        else
-          self
-        end
+        respond_to?(:collection) ? collection : self
       end
 
     end
