6c9c37598afab70ba8647b1e62f48e597c544243
Domizio Demichelis <dd.nexus@gmail.com>
2013-01-30 12:49:11 +0100

    simpler LIB_PATHS assignation

--- lib/flex-scopes.rb
+++ lib/flex-scopes.rb
@@ -3,6 +3,6 @@ require 'flex/scope'
 require 'flex/scopes'
 require 'flex/result/scope'
 
-Flex::LIB_PATHS << __FILE__.sub(/flex-scopes.rb$/, '')
+Flex::LIB_PATHS << File.dirname(__FILE__)
 
 Flex::Conf.result_extenders |= [Flex::Result::Scope]
